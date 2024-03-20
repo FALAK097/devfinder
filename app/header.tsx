e5e14896c7366d1a67f2988d957e8393796b7946
falak <falakgala09@gmail.com>
2024-03-20 20:05:16 +0530

    metadata & header styling

--- app/header.tsx
+++ app/header.tsx
@@ -1,22 +1,52 @@
 'use client';
 
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 import { ModeToggle } from '@/components/mode-toggle';
 import { Button } from '@/components/ui/button';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { LogInIcon, LogOutIcon } from 'lucide-react';
+
+function AccountDropdown() {
+  const session = useSession();
+  const isLoggedIn = !!session.data;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger> {session.data?.user?.name}</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon /> Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn('google')}>
+            <LogInIcon /> Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 export function Header() {
   const session = useSession();
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn('google')}>Sign In</Button>
-        )}
-        <ModeToggle />
-        {session.data?.user?.name}
+    <header className="bg-gray-100 dark:bg-gray-900 py-4 container mx-auto">
+      <div className="flex justify-between items-center">
+        <div>LOGO</div>
+        <div>
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
